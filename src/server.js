53e21e2b80611766bfead83ae6a9ec152c694295
ttluan010103 <l2team.contact@gmail.com>
2025-08-25 14:42:38 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-app.use(cors({origin  : 'http://localhost:5173'}))
+app.use(cors({origin  : 'https://deploy-client-fawn.vercel.app'}))
 
 app.get('/api/init', (req, res) => {
     return res.status(200).json({       
